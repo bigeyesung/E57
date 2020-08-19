43a4f29d5f327ed198e42a3367100eea47035010
chenhsi <sungchenhsi@gmail.com>
2020-08-19 23:49:11 +0100

    static lib setting

---
+++ build/CMakeFiles/MyExecutable.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyExecutable.dir/src/main2.cpp.o"
+  "MyExecutable.pdb"
+  "MyExecutable"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyExecutable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main2.cpp
@@ -0,0 +1,9 @@
+#include "static/Hello.h"
+#include "iostream"
+int main(int argc, char *argv[])
+{
+    Hello hi;
+    hi.print();
+    std::cout<<"OMG"<<std::endl;
+    return 0;
+}
